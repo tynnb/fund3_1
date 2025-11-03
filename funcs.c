9a50ce6bdbea3ab6fdda96d33aacf97eac5cf885
tynnb <tynbb084@gmail.com>
2025-11-03 23:55:59 +0300

    conversion number

--- funcs.c
+++ funcs.c
@@ -23,3 +23,68 @@ int count_digits(int number, int base) {
     }
     return count;
 }
+
+char get_digit_char(int value) {
+    if (value < 10) {
+        return '0' + value;
+    }
+    else {
+        return 'A' + (value - 10);
+    }
+}
+
+char* dec_to_base2r(int number, int r) {
+    if (!validate_parameters(r)) {
+        return NULL;
+    }
+
+    int base_shift = r;
+    int base = 1 << r;
+    int mask = base - 1;
+
+    if (number == 0) {
+        char* result = (char*)malloc(2 * sizeof(char));
+        result[0] = '0';
+        result[1] = '\0';
+        return result;
+    }
+
+    int is_negative = (number >> (sizeof(int) * 8 - 1)) & 1;
+    int abs_number = custom_abs(number);
+    int digit_count = count_digits(number, base_shift);
+
+    int sign_length = 0;
+    if (is_negative) {
+        sign_length = 1;
+    }
+    
+    int length = digit_count + sign_length + 1;
+    char* result = (char*)malloc(length * sizeof(char));
+
+    if (result == NULL) {
+        return NULL;
+    }
+
+    int index = digit_count + sign_length;
+    result[index] = '\0';
+    index--;
+    int temp = abs_number;
+    
+    while (temp != 0) {
+        int digit = temp & mask;
+        result[index] = get_digit_char(digit);
+        index--;
+        temp = temp >> base_shift;
+    }
+
+    if (is_negative) {
+        result[0] = '-';
+    }
+    return result;
+}
+
+void free_string(char* string) {
+    if (string != NULL) {
+        free(string);
+    }
+}
